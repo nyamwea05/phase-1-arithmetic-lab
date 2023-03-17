f553f029d73a0f25863c095586907012a923ea08
sandra <nyamweasandra@gmail.com>
2023-03-17 11:39:12 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+
+let multiply = num1*num2;
+console.log(multiply);
+
+let random = Math.ceil(Math.random());
+console .log(random);
+let num3 =22
+let num4 =6
+
+const mod =num3% num4;
+console.log(mod);
+
+let max =(1,2,4,20)
+Console.log(Math.max(max))
